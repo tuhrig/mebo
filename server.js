20fce5df44f953b8ae124d22607a9e33c0337d42
Thomas Uhrig <thomas.uhrig@westernacher.com>
2016-12-27 16:03:34 +0100

    Moved body parser config to server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var fs = require('fs');
 var log4js = require( "log4js" );
+var bodyParser = require('body-parser');
 
 var logger = log4js.getLogger("app");
 
@@ -10,6 +11,7 @@ logger.info("..--##≤≤ ,,..--..,, >>##--..");
 
 var app = express();
 app.use(express.static(__dirname)); //Serves resources from public folder
+app.use(bodyParser.json());
 
 var boardRouter = require('./src/routes/board.router.js');
 var messageRouter = require('./src/routes/message.router.js');
--- src/routes/message.router.js
+++ src/routes/message.router.js
@@ -1,11 +1,7 @@
 var express = require('express');
 var boardService = require('../services/board.service.js');
-var bodyParser = require('body-parser');
 
 var router = express.Router();
-
-router.use(bodyParser.json()); // support json encoded bodies
-router.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 router.get('/boards/:id/messages', function(req, res) {
     var id = req.params.id;
